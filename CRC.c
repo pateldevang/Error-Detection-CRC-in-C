dc10f45b402b9250417b8ee6a6c67be0823952ff
Devang <45505129+pateldevang@users.noreply.github.com>
2020-02-05 22:54:50 +0530

    indent

--- CRC.c
+++ CRC.c
@@ -9,15 +9,19 @@ int main()
 	scanf("%d",&m);
 	int frm[n+m-1],grm[m],a[m],chk[n+m-1],t[n];
 	printf("Enter the frame:");
-	for(i=0;i<n;i++){
+	for(i=0;i<n;i++)
+	{
 		scanf("%d",&frm[i]);
 		chk[i]=frm[i];
 		t[i]=frm[i];
-		}
-	for(i=n;i<n+m-1;i++){
-		frm[i]=0;}
+	}
+	for(i=n;i<n+m-1;i++)
+	{
+		frm[i]=0;
+	}
 	printf("Enter the Divisor: ");
-	for(i=0;i<m;i++){
+	for(i=0;i<m;i++)
+	{
 		scanf("%d",&grm[i]);
 		a[i]=0;
 	}
@@ -40,16 +44,19 @@ int main()
 		}
 	}
 	printf("Remainder is: ");
-	for(i=n;i<n+m-1;i++){
+	for(i=n;i<n+m-1;i++)
+	{
 		printf("%d",frm[i]);
-		chk[i]=frm[i];}
+		chk[i]=frm[i];
+	}
 	printf("\n");
 	printf("Codeword generated is  = ");
 	for(i=0;i<n;i++)
 	{
 		printf("%d",t[i]);
 	}
-	for(i=n;i<n+m-1;i++){
+	for(i=n;i<n+m-1;i++)
+	{
 		printf("%d",frm[i]);
 		chk[i]=frm[i];}
 		for(i=0;i<n;i++)
